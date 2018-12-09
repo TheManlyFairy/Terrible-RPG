817d83de6a3f78a214b1bcaa7f6c65b3a5375280
Nati <33567785+DevilN31@users.noreply.github.com>
2018-12-10 00:03:50 +0200

    Character selection Menue

---
+++ Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Character/Stats")]
+public class CharacterStats : ScriptableObject
+{
+    public string characterName;
+    public float maxHealth;
+    public float health;
+    public float mana;
+    public float strength;
+    public float defense;
+    public float intelligence;
+    public float agility;
+
+    public void Reset()
+    {
+        health = maxHealth;
+    }
+    public float TakeDamage(CharacterStats attacker)
+    {
+        return health -= (attacker.strength - defense);
+    }
+
+    public override string ToString()
+    {
+        return "Name: " + characterName + "\nMax Health: " + maxHealth + "\nMana: " + mana + "\nStrenth: " + strength 
+            + "\nDefence: " + defense + "\nIntelligence: " + intelligence + "\nAgility: " + agility;
+    }
+}
---
+++ Assets/Scripts/UI/UIButtonManager.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+
+
+public class UIButtonManager : MonoBehaviour {
+
+    private GameObject startMenuPanel;
+    private GameObject aboutPanel;
+    private GameObject optionsPanel;
+    private GameObject characterSelectPanel;
+
+    public GameObject currentPanel;
+    public GameObject previousPanel;
+
+    public Text characterStatsText;
+    public Text characterLore;
+
+    private void Awake()
+    {
+        startMenuPanel = GameObject.Find("MainMenuePanel");
+        optionsPanel = GameObject.Find("SettingsMenuePanel");
+        optionsPanel.SetActive(false);
+        characterSelectPanel = GameObject.Find("CharacterSelectPanel");
+        characterSelectPanel.SetActive(false);
+       
+    }
+
+
+    public void OnButtonClick()
+    {
+        string name = EventSystem.current.currentSelectedGameObject.name;
+        switch (name)
+        {
+            case "NewGameButton":
+                OnNewGameButtonClick();
+                currentPanel = characterSelectPanel;
+                previousPanel = startMenuPanel;
+                break;
+
+            case "SettingsButton":
+                Debug.Log(name);
+                OnOptionsButtonClick();
+                currentPanel = optionsPanel;
+                previousPanel = startMenuPanel;
+                break;
+
+            case "AboutBtn":
+                Debug.Log(name);
+                OnAboutButtonClick();
+                break;
+
+            case "BackButton":
+                OnBackButtonClick(currentPanel,previousPanel);
+                break;
+
+            case "LoadGameButton":
+                OnLoagGameButtonClick();
+                break;
+
+            case "NunButton":
+                OnHeroButtonClick();
+                break;
+
+            case "FatButton":
+                OnHeroButtonClick();
+                break;
+
+            case "SushiButton":
+                OnHeroButtonClick();
+                break;
+
+        }
+    }
+
+    private void OnNewGameButtonClick()
+    {
+        //Starts a new game go to Character Select page 
+
+        startMenuPanel.SetActive(false);
+        characterSelectPanel.SetActive(true);
+      //  OnHeroButtonClick("Hero 1");
+
+    }
+
+    private void OnBackButtonClick(GameObject currenPanel, GameObject previousPanel)
+    {
+        currentPanel.SetActive(false);
+        previousPanel.SetActive(true);
+    }
+
+    private void OnAboutButtonClick()
+    {
+        aboutPanel.SetActive(true);
+        startMenuPanel.SetActive(false);
+        Debug.Log(aboutPanel.name);
+
+    }
+
+    private void OnOptionsButtonClick()
+    {
+        optionsPanel.SetActive(true);
+        startMenuPanel.SetActive(false);
+    }
+
+    
+    private void OnHeroButtonClick()
+    {
+        characterStatsText.text = EventSystem.current.currentSelectedGameObject.GetComponent<CharacterInfo>().characterStats.ToString();
+       characterLore.text =  ReadString(EventSystem.current.currentSelectedGameObject.GetComponent<CharacterInfo>().characterStats.characterName);
+    }
+    
+
+    private void OnLoagGameButtonClick()
+    {
+        if(SceneManager.GetSceneByName("GameTestScene") != null)
+        SceneManager.LoadScene("GameTestScene");
+    }
+
+   string ReadString( string characterLoreName)
+    {
+        string path = "Assets/Characters/" + characterLoreName + ".txt";
+
+        //Read the text from directly from the test.txt file
+        StreamReader reader = new StreamReader(path);
+        string characterLore = reader.ReadToEnd();
+        reader.Close();
+
+        return characterLore.ToString();
+    }
+
+}
