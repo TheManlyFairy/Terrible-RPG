67b3e8cdd566fb460b7e4d98a36ddc7cf03e45ea
Nati <33567785+DevilN31@users.noreply.github.com>
2019-01-16 16:49:45 +0200

    World map & Minimap UI

---
+++ Assets/Scripts/UI/World/TestPlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayerMovement : MonoBehaviour {
+
+    public int movementSpeed = 10;
+    public int rotationSpeed = 20;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticleInput = Input.GetAxis("Vertical");
+
+        if (verticleInput != 0)
+        {
+            transform.position += transform.forward * movementSpeed * verticleInput * Time.deltaTime;
+        }
+
+        if(horizontalInput != 0)
+        {
+            transform.Rotate(Vector3.up * rotationSpeed * horizontalInput);
+        }
+    }
+}
