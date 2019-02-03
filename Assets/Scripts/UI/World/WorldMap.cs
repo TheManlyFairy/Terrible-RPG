1853d81ac824dbf301aaa11212058f2676d629a8
Nati <33567785+DevilN31@users.noreply.github.com>
2019-02-03 09:39:29 +0200

    UI update

---
+++ Assets/Scripts/UI/World/WorldMap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WorldMap : MonoBehaviour {
+
+    public RawImage worldMapImage;
+    
+    void Start()
+    {
+        worldMapImage = GetComponent<RawImage>();
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.Tab))
+        {
+            worldMapImage.enabled = true;
+        }
+        else
+        worldMapImage.enabled = false;
+
+    }
+}
